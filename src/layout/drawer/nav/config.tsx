bd9a6cfbcf8f25bdb1df24e7dd57ceaabb064540
Mirosław Kołodziej <80774381+mirek0707@users.noreply.github.com>
2023-03-18 20:06:53 +0100

    CTB-42 Drawer

---
+++ src/layout/drawer/nav/config.tsx
@@ -0,0 +1,19 @@
+import AnalyticsIcon from '@mui/icons-material/Analytics'
+import DashboardIcon from '@mui/icons-material/Dashboard'
+
+import Routes from 'routes'
+
+import { ListItemProps } from './types'
+
+export const LayoutDrawerNavConfig: Array<ListItemProps> = [
+  {
+    name: 'Dashboard',
+    path: Routes.DashboardUrl(),
+    icon: <DashboardIcon />,
+  },
+  {
+    name: 'Statistics',
+    path: Routes.StatisticsUrl(),
+    icon: <AnalyticsIcon />,
+  },
+]
---
+++ src/layout/drawer/nav/index.tsx
@@ -0,0 +1,18 @@
+import List from '@mui/material/List'
+import React from 'react'
+
+import { LayoutDrawerNavConfig } from './config'
+import LayoutDrawerNavItem from './item'
+import { ListItemProps } from './types'
+
+const LayoutDrawerNav: React.FC = () => {
+  return (
+    <List>
+      {LayoutDrawerNavConfig.map((item: ListItemProps) => (
+        <LayoutDrawerNavItem key={item.name} {...item} />
+      ))}
+    </List>
+  )
+}
+
+export default LayoutDrawerNav
