815bdc7c93bf239bd8ec6400a2566cc7b95496ba
wiktor <example@email.com>
2023-03-19 21:04:36 +0100

    Add table component and sample data.

--- src/sandbox/wiktor/index.tsx
+++ src/sandbox/wiktor/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import Table from './HistoryTable';
+import data from './sampleHistoryTableData';
+
 const WiktorSandboxPage: React.FC = () => {
   return (
     <>
       <h1>Wiktor Sandbox</h1>
+      <Table data={data} />
     </>
   );
 };
